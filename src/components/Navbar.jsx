20b86cadce5405eda3b590f3145f32334516bc29
hhoow0093 <howarddai2005@gmail.com>
2025-01-08 16:22:46 +0700

    menyicil navbar portfolio web menggunakan reactJS

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,189 @@
+import React, { useState } from "react";
+import "../css/navbar.css";
+import Logo from "../assets/svg/valorant.svg";
+
+/*
+isi portfolio page
+0. navbar
+0.1 Hero section
+1. About me
+2. My value
+3. Technical skills
+4. Credits To
+5. Portfolio
+6. Footer
+*/
+
+function Navbar() {
+  const [showNavbarText, setShowNavbarText] = useState(false);
+  const [showText, setShowText] = useState(false);
+  const [isClicking, setClicking] = useState(false);
+
+  const handleButtonClick = () => {
+    if (isClicking) {
+      return;
+    }
+    setClicking(true);
+
+    if (showNavbarText === false) {
+      setShowNavbarText(true);
+      setTimeout(() => {
+        setShowText(true);
+        setClicking(false);
+      }, 500);
+    } else if (showNavbarText === true) {
+      setShowNavbarText(false);
+      setShowText(false);
+      setTimeout(() => {
+        setClicking(false);
+      }, 500);
+    }
+  };
+
+  return (
+    <>
+      <div
+        className={`navbar-valo ${showNavbarText ? "show-navbar-text" : ""}`}
+      >
+        <div className="nav-header container-fluid">
+          <button className="click-logo" onClick={handleButtonClick}>
+            <div className="logo">
+              <svg
+                fill="#fff"
+                width="50px"
+                height="50px"
+                viewBox="0 0 32 32"
+                id="Camada_1"
+                version="1.1"
+                xml:space="preserve"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+              >
+                <g>
+                  <path d="M19.8,26.1h-0.2c-2.4,0-4.8,0-7.2,0c-0.3,0-0.5-0.1-0.6-0.3c-2.5-3.2-5.1-6.3-7.6-9.5C4.1,16.1,4,16,4,15.8   c0-3.1,0-6.1,0-9.2c0-0.1,0-0.2,0.1-0.2h0.1c5.2,6.5,10.4,13,15.5,19.5c0,0,0,0.1,0.1,0.1L19.8,26.1L19.8,26.1z" />
+
+                  <path d="M27.8,16.3c-0.7,0.9-1.5,1.8-2.2,2.8c-0.2,0.2-0.4,0.3-0.6,0.3c-2.4,0-4.8,0-7.1,0c0,0-0.1,0-0.1,0c-0.1,0-0.2-0.1-0.1-0.2   c0,0,0-0.1,0.1-0.1c2.4-3,4.7-5.9,7.1-8.9c1-1.2,2-2.5,2.9-3.7c0-0.1,0.1-0.1,0.2-0.1c0,0,0.1,0,0.1,0c0,0.1,0,0.1,0,0.2   c0,3,0,6.1,0,9.1C28,16,27.9,16.2,27.8,16.3L27.8,16.3z" />
+                </g>
+              </svg>
+            </div>
+          </button>
+          {showText && (
+            <p className="fixed-title">
+              <a href="">My Portfolio Web</a>
+            </p>
+          )}
+        </div>
+        <div className="my-navigation container-fluid">
+          <ul className="my-unordered-list">
+            <li>
+              <span className="my-icon">
+                <svg
+                  width="30px"
+                  height="30px"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 6V15H6V11C6 9.89543 6.89543 9 8 9C9.10457 9 10 9.89543 10 11V15H15V6L8 0L1 6Z"
+                    fill="#ffff"
+                    id="home"
+                  />
+                </svg>
+              </span>
+              {showText && <a href="">About me</a>}
+            </li>
+            <li>
+              <span className="my-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-code-slash text-white"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0m6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0" />
+                </svg>
+              </span>
+              {showText && <a href="">My Value</a>}
+            </li>
+            <li>
+              <span className="my-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-laptop-fill text-white"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2.5 2A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2zM0 12.5h16a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5" />
+                </svg>
+              </span>
+              {showText && <a href="">Technical Skills</a>}
+            </li>
+            <li>
+              <span className="my-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-heart-fill text-white"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+                  />
+                </svg>
+              </span>
+              {showText && <a href="">Credits To</a>}
+            </li>
+            <li>
+              <span className="my-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  class="bi bi-list-columns-reverse text-white"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M0 .5A.5.5 0 0 1 .5 0h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 .5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10A.5.5 0 0 1 4 .5m-4 2A.5.5 0 0 1 .5 2h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 4h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 6h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 8h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"
+                  />
+                </svg>
+              </span>
+              {showText && <a href="">Portfolio</a>}
+            </li>
+          </ul>
+        </div>
+        <div className="my-navigation-footer container-fluid">
+          <div className="row  my-footer-nav py-3">
+            <div className="my-col col-4">
+              <span className="my-icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  fill="currentColor"
+                  class="bi bi-envelope-at-fill text-white"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2zm-2 9.8V4.698l5.803 3.546zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.5 4.5 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586zM16 9.671V4.697l-5.803 3.546.338.208A4.5 4.5 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671" />
+                  <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791" />
+                </svg>
+              </span>
+            </div>
+            <div className="my-col col-8 d-flex align-items-end">{showText && <a href="" className="contact-me">Contact me!</a>}</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
